e00c12abecb142ddaae46a58d235a2eb7bff3eae
As-bu <asbu666@pm.me>
2025-05-04 17:54:39 +0200

    added markdown splitting functionality and tests

--- src/test_util.py
+++ src/test_util.py
@@ -2,31 +2,90 @@ import unittest
 
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from util import text_to_html
+from util import tnode_to_hnode, split_nodes_delimiter
 
 
 class test_node_to_node(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.NORMAL)
-        html_node = text_node_to_html_node(node)
+        html_node = tnode_to_hnode(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
     def test_italic(self):
         node = TextNode('Italian food is great', TextType.ITALIC)
-        html_node = text_node_to_html_node(node)
+        html_node = tnode_to_hnode(node)
         self.assertEqual(html_node.tag, 'i')
         self.assertEqual(html_node.value, 'Italian food is great')
 
     def test_link(self):
         node = TextNode('This is a link(to the past?)', TextType.LINK, 'https://boot.dev')
-        html_node = text_node_to_html_node(node)
+        html_node = tnode_to_hnode(node)
         self.assertEqual(html_node.tag, 'a')
         self.assertEqual(html_node.value, 'This is a link(to the past?)')
         self.assertEqual(
             html_node.props,
             {'href': 'https://boot.dev'},
         )
+
+class test_split_nodes_delimiter(unittest.TestCase):
+    def test_wrong_delim(self):
+        with self.assertRaises(Exception) as context:
+            node = TextNode("This is a text with a `code` like it's the Matrix", TextType.NORMAL)
+            new_nodes = split_nodes_delimiter([node], '_', TextType.CODE)
+    
+    def test_delim_italic(self):
+        node = TextNode("This is not _Italian_, you should know that by now!", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], '_', TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("This is not ", TextType.NORMAL),
+                TextNode("Italian", TextType.ITALIC),
+                TextNode(", you should know that by now!", TextType.NORMAL),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_multi_bold(self):
+        node = TextNode("In my native language it's not **bold** it's **FAT**, weird, right?", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("In my native language it's not ", TextType.NORMAL),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" it's ", TextType.NORMAL),
+                TextNode("FAT", TextType.BOLD),
+                TextNode(", weird, right?", TextType.NORMAL),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_node(self):
+        node = TextNode("THIS IS A BOLD MESSAGE IN A BOLD NODE FROM A BALD MAN", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("THIS IS A BOLD MESSAGE IN A BOLD NODE FROM A BALD MAN", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_and_code(self):
+        node = TextNode("It ain't much, **but** it's honest `code`", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, '`', TextType.CODE)
+        self.assertListEqual(
+            [
+                TextNode("It ain't much, ", TextType.NORMAL),
+                TextNode("but", TextType.BOLD),
+                TextNode(" it's honest ", TextType.NORMAL),
+                TextNode("code", TextType.CODE),
+            ],
+            new_nodes,
+        )
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- src/util.py
+++ src/util.py
@@ -1,24 +1,71 @@
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode, ParentNode, LeafNode
 
-class text_to_html:
-    def text_node_to_html_node(text_node):
-        if text_node.text_type == TextType.NORMAL:
-            return LeafNode(None, text_node.text)
+#Constant dict for TextType/Delim, add more as needed
+DELIMITER_TO_TEXT_TYPE = {
+        '**': TextType.BOLD,
+        '_': TextType.ITALIC,
+        '`': TextType.CODE,
+}
 
-        if text_node.text_type == TextType.BOLD:
-            return LeafNode('b', text_node.text)
+def tnode_to_hnode(text_node):
+    if text_node.text_type == TextType.NORMAL:
+        return LeafNode(None, text_node.text)
 
-        if text_node.text_type == TextType.ITALIC:
-            return LeafNode('i', text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode('b', text_node.text)
 
-        if text_node.text_type == TextType.CODE:
-            return LeafNode('code', text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode('i', text_node.text)
 
-        if text_node.text_type == TextType.LINK:
-            return LeafNode('a', text_node.text, {'href': text_node.url})
+    if text_node.text_type == TextType.CODE:
+        return LeafNode('code', text_node.text)
 
-        if text_node.text_type == TextType.IMAGE:
-            return LeafNode('img', '', {'src': text_node.url, 'alt': text_node.text})
+    if text_node.text_type == TextType.LINK:
+        return LeafNode('a', text_node.text, {'href': text_node.url})
 
-        raise Exception('Unsupported text type')
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode('img', '', {'src': text_node.url, 'alt': text_node.text})
+
+    raise Exception("Unsupported text type")
+
+
+def split_nodes_delimiter(old_nodes, delim, text_type):
+    if delim in DELIMITER_TO_TEXT_TYPE and DELIMITER_TO_TEXT_TYPE[delim] != text_type:
+        raise Exception(f"Delimiter {delim} does not match text type {text_type}")
+
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+            continue
+
+        splt_node = node.text.split(delim)
+        new_nodes_from_splt = []
+        
+        if len(splt_node) == 1:
+            new_nodes.append(node)
+            continue
+
+        if len(splt_node) % 2 == 0:
+            raise Exception("The node has a missing delimiter")
+
+        for i in range(len(splt_node)):
+            if splt_node[i] == '':
+                continue
+            if i % 2 == 0:
+                new_nodes_from_splt.append(TextNode(splt_node[i], TextType.NORMAL))
+            else:
+                new_nodes_from_splt.append(TextNode(splt_node[i], text_type))
+        
+        new_nodes.extend(new_nodes_from_splt)
+
+    return new_nodes
+
+    
+
+            
+
+
+
+
