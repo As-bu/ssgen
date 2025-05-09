679ac185779c43dbc27dbdae3f20af2866fa6382
As-bu <asbu666@pm.me>
2025-05-09 18:18:43 +0200

    added block to blocktype conversion

--- src/block_transformers.py
+++ src/block_transformers.py
@@ -1,4 +1,6 @@
 import re
+from enum import Enum
+
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode, ParentNode, LeafNode
 
@@ -13,4 +15,35 @@ def markdown_to_blocks(markdown):
         text_blocks.append(cleanup)
 
     return text_blocks
+
+class BlockType(Enum):
+    PARA = 'paragraph'
+    HEAD = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    ULIST = 'unordered list'
+    OLIST = 'ordered list'
+
+def block_to_blocktype(textblock):
+    if re.search(r'\# (.*)', textblock):
+        return BlockType.HEAD
+    elif re.search(r'\`(.*?)\`', textblock):
+        return BlockType.CODE
+    elif re.search(r'\>(.*)', textblock):
+        return BlockType.QUOTE
+    elif re.search(r'\- (.*)', textblock):
+        return BlockType.ULIST
+    elif re.search(r'\d+\. (.*)', textblock):
+        matches = re.findall(r'\d+. (.*)', textblock, re.MULTILINE)
+
+        for i, match in enumerate(matches, 1):
+            line = f'{i}. {match}'
+            number = int(line.split('.')[0])
+            if number != i:
+                raise Exception("Formatting error, the list indices are not in order")
+            else:
+                return BlockType.OLIST
+    else:
+        return BlockType.PARA
+
 
--- src/test_block_transformers.py
+++ src/test_block_transformers.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextType, TextNode, tnode_to_hnode
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from block_transformers import markdown_to_blocks
+from block_transformers import markdown_to_blocks, BlockType, block_to_blocktype
 
 
 class test_markdown_to_blocks(unittest.TestCase):
@@ -69,6 +69,61 @@ It's _really_ annoying **!!!!!!** Here's some reasons:
         )
 
 
+class test_block_to_blocktypes(unittest.TestCase):
+
+    def test_block_is_para(self):
+        textblock = "This is just text to test this function"
+        transform = block_to_blocktype(textblock)
+        self.assertEqual(
+            transform,
+            BlockType.PARA,
+        )
+
+    def test_block_is_heading(self):
+        textblock = "### This is a heading."
+        transform = block_to_blocktype(textblock)
+        self.assertEqual(
+            transform,
+            BlockType.HEAD,
+        )
+
+    def test_block_is_code(self):
+        textblock = "```This might not look like it, but it's a bunch of code.```"
+        transform = block_to_blocktype(textblock)
+        self.assertEqual(
+            transform,
+            BlockType.CODE,
+        )
+
+    def test_block_is_quote(self):
+        textblock = ">This is a quote of a famous person or something.\n>Really!"
+        transform = block_to_blocktype(textblock)
+        self.assertEqual(
+            transform,
+            BlockType.QUOTE,
+        )
+
+    def test_block_is_ulist(self):
+        textblock = "- This is a list of tests.\n- I hope it works.\n- We won't know until I run them."
+        transform = block_to_blocktype(textblock)
+        self.assertEqual(
+            transform,
+            BlockType.ULIST,
+        )
+
+    def test_block_is_olist(self):
+        textblock = "1. This is an ordered list.\n2. You can tell, because it uses numbers.\n3. That's pretty much it."
+        transform = block_to_blocktype(textblock)
+        self.assertEqual(
+            transform,
+            BlockType.OLIST,
+        )
+
+    def test_block_is_olist_error(self):
+        with self.assertRaises(Exception) as context:
+            textblock = "2. This should throw an error.\n1. I hope.\n3. Please don't let me down."
+            transform = block_to_blocktype(textblock)
+            self.assertTrue("Formatting error, the list indices are not in order" in context.exception)
 
 if __name__ == '__main__':
     unittest.main()
